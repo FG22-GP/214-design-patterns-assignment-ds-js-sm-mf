859830249be560b488fd311f59e6f31bedc9d598
technove300 <max.fagerstrom@hotmail.com>
2023-12-05 17:14:30 +0100

    graphics lib wip

---
+++ GameEngine/Graphics.cpp
@@ -0,0 +1,148 @@
+#include "Graphics.h"
+#include <set>
+
+std::set<RenderObject*> RenderObjects;
+SDL_Renderer* renderer = NULL;
+TTF_Font* font = NULL;
+SDL_Window* window = NULL;
+
+int screen_width;
+int screen_height;
+
+bool initDone = false;
+
+void GraphicsLib::ClearFrame()
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+}
+
+void GraphicsLib::PresentFrame()
+{
+	SDL_RenderPresent(renderer);
+}
+
+bool GraphicsLib::Initialize(const char* windowTitle, int Width, int Height)
+{
+	if (initDone) return false;
+
+	screen_width = Width;
+	screen_height = Height;
+
+	bool ok = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		ok = false;
+	}
+
+	if (TTF_Init() == -1)
+	{
+		printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+		ok = false;
+	}
+
+	/*font = TTF_OpenFont("font.ttf", 28);
+	if (font == NULL)
+	{
+		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+		ok = false;
+	}*/
+
+	if (ok)
+	{
+		window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+		ok = renderer;
+	}
+
+	if (ok)
+	{
+		initDone = true;
+		printf("graphics lib init ok\n");
+	}
+
+	return ok;
+}
+
+void GraphicsLib::Exit()
+{
+	if (initDone)
+	{
+		SDL_DestroyRenderer(renderer);
+		renderer = NULL;
+		SDL_DestroyWindow(window);
+		window = NULL;
+		SDL_Quit();
+		initDone = false;
+	}
+}
+
+RenderObject::RenderObject()
+{
+	RenderObjects.insert(this);
+	Hide = false;
+}
+
+RenderObject::~RenderObject()
+{
+	RenderObjects.erase(this);
+}
+
+void GraphicsLib::RenderSquare(int x, int y, int width, int height, unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+{
+	SDL_SetRenderDrawColor(renderer, r, g, b, a);
+	SDL_Rect rect = SDL_Rect(x, y, width, height);
+	SDL_RenderFillRect(renderer, &rect);
+}
+
+void GraphicsLib::RenderCircle(int x, int y, int radius, unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+{
+	SDL_SetRenderDrawColor(renderer, r, g, b, a);
+
+
+	//https://gist.github.com/Gumichan01/332c26f6197a432db91cc4327fcabb1c
+
+	int offsetx, offsety, d;
+	int status;
+
+	offsetx = 0;
+	offsety = radius;
+	d = radius - 1;
+	status = 0;
+
+	while (offsety >= offsetx) {
+
+		status += SDL_RenderDrawLine(renderer, x - offsety, y + offsetx,
+			x + offsety, y + offsetx);
+		status += SDL_RenderDrawLine(renderer, x - offsetx, y + offsety,
+			x + offsetx, y + offsety);
+		status += SDL_RenderDrawLine(renderer, x - offsetx, y - offsety,
+			x + offsetx, y - offsety);
+		status += SDL_RenderDrawLine(renderer, x - offsety, y - offsetx,
+			x + offsety, y - offsetx);
+
+		if (status < 0) {
+			status = -1;
+			break;
+		}
+
+		if (d >= 2 * offsetx) {
+			d -= 2 * offsetx + 1;
+			offsetx += 1;
+		}
+		else if (d < 2 * (radius - offsety)) {
+			d += 2 * offsety - 1;
+			offsety -= 1;
+		}
+		else {
+			d += 2 * (offsety - offsetx - 1);
+			offsety -= 1;
+			offsetx += 1;
+		}
+	}
+
+}
---
+++ GameEngine/Graphics.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+class RenderObject;
+
+class GraphicsLib
+{
+public:
+	static bool Initialize(const char* windowTitle, int Width = 640, int Height = 480);
+	static void Exit();
+	static void PresentFrame(void);
+	static void ClearFrame(void);
+	static void RenderCircle(int x, int y, int radius, unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 0);
+	static void RenderSquare(int x, int y, int width, int height, unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 0);
+};
+
+class RenderObject
+{
+public:
+	RenderObject();
+	~RenderObject();
+
+	virtual void Render() = 0;
+
+	bool Hide;
+};
+
+class Sprite : public RenderObject
+{
+public:
+	void Render();
+
+	void Set(int x, int y, double angle = 0.0, int textureIndex = -1, SDL_Point center = { 32, 32 }, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	void Set(int textureIndex);
+
+	int textureIdx;
+
+private:
+	SDL_Texture* texture;
+	SDL_Rect renderQuad;
+	SDL_Rect clip;
+	double angle;
+	SDL_Point center;
+	SDL_RendererFlip flip;
+
+};
+
+class Rectangle : public RenderObject
+{
+public:
+	void Render();
+
+	void SetPos(int x, int y, int width, int height);
+	void SetSize(int Width, int Height);
+	void SetColor(unsigned char r, unsigned char g, unsigned char b);
+
+private:
+	SDL_Rect renderQuad;
+	SDL_Color renderColor;
+
+};
+
+class Circle : public RenderObject
+{
+public:
+	void Render();
+
+	void SetPos(int x, int y, int width, int height);
+	void SetSize(int Width, int Height);
+	void SetColor(unsigned char r, unsigned char g, unsigned char b);
+
+private:
+	SDL_Rect renderQuad;
+	SDL_Color renderColor;
+
+};
+
+class Text : public RenderObject
+{
+public:
+	Text();
+	~Text();
+
+	void Render();
+
+	void Set(int x, int y);
+	void Set(const char* text);
+
+private:
+	SDL_Texture* fontTexture;
+	SDL_Rect renderQuad;
+	int width;
+	int height;
+};
